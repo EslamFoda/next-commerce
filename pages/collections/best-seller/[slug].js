b0691b4870ed38c0c3a6974c8185304cf457b8a6
eslamfoda <eslaamfodaa@gmail.com>
2022-07-22 17:56:09 +0300

    fix deploy error

--- pages/collections/best-seller/[slug].js
+++ pages/collections/best-seller/[slug].js
@@ -5,21 +5,18 @@ import RecentlyView from "../../../comp/product-page/recentlyViewProd";
 import { useAppContext } from "../../../context/AppContext";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-const bestSellersId = ({ product }) => {
+const BestSellersId = ({ product }) => {
   const { storeProducts } = product;
   const router = useRouter();
   const { slug } = router.query;
   const { seenProducts, dispatch } = useAppContext();
   useEffect(() => {
-    console.log(slug);
-    console.log(seenProducts);
-
     const filterDublicatProd = seenProducts.filter((prod) => {
       return prod.slug !== slug;
     });
 
     dispatch({ type: "REMOVE_DUBLICATES", payload: filterDublicatProd });
-  }, []);
+  }, [seenProducts]);
   return (
     <>
       <Breadcrumb type={storeProducts[0]?.type} slug={storeProducts[0]?.slug} />
@@ -29,7 +26,7 @@ const bestSellersId = ({ product }) => {
   );
 };
 
-export default bestSellersId;
+export default BestSellersId;
 
 export async function getServerSideProps({ params }) {
   const product = (await getSingleProduct(params.slug, 8)) || [];
