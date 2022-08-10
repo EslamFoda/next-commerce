926810bee22a63a4d8226e322feedbaa602999ec
eslamfoda <eslaamfodaa@gmail.com>
2022-08-11 01:06:23 +0300

    add grid items

--- comp/collections/collectionView.tsx
+++ comp/collections/collectionView.tsx
@@ -1,4 +1,6 @@
 import ProdCard from "../common/prodCard"
+import { ViewListIcon} from "@heroicons/react/solid"
+import { Grid4,Grid3,Grid2,List } from "../icons"
 const CollectionView = ({type,collection})=>{
     const {storeProducts} = collection
     return (<div className="grid grid-cols-4 gap-5 h-screen p-4">
@@ -6,8 +8,14 @@ const CollectionView = ({type,collection})=>{
         <div className=" col-span-3">
             <div className="flex items-center justify-between">
             <h1>{type} ({storeProducts?.length})</h1>
-            <div>
-                icons
+            <div className="flex items-center gap-2">
+            
+               <List/>
+               <Grid2/>
+               <Grid3/>
+               <div style={{marginBottom:"-2px"}}>
+               <Grid4/>
+               </div>
             </div>
             </div>
             <div className="grid grid-cols-3 gap-5">
