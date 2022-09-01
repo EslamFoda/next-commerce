e842fe4cfeb72c92c5015a01a2cd8a34f2e6bb14
eslamfoda <eslaamfodaa@gmail.com>
2022-09-01 14:45:04 +0300

    debug

--- comp/mainCard/gridCard.tsx
+++ comp/mainCard/gridCard.tsx
@@ -12,7 +12,16 @@ const GridCard = ({ pushTo, selectProd, setSelectedProd, addToCart, prod }) => {
       {" "}
       <div className="overflow-hidden group h-[450px] lg:h-[550px] md:h-[550px] sm:h-[550px]">
         <div className="top cursor-pointer  relative h-64 lg:h-96 md:h-96 sm:h-96">
-         
+          <Image
+            alt={title}
+            onClick={pushTo}
+            // @ts-ignore
+            src={selectProd ? selectProd : prodImages[0].url}
+            layout="fill"
+            objectFit="cover"
+            // @ts-ignore
+          />
+          <ViewAndWishlist prod={prod} />
         </div>
         <div className="buttom group-hover:translate-y-[-60px] lg:translate-y-0 md:translate-y-0 translate-y-[-60px]  transition duration-300 pt-3 bg-white flex items-center justify-center flex-col gap-1">
           <span className="text-gray-500 transition-all hover:text-primary-hover text-sm">
@@ -29,7 +38,11 @@ const GridCard = ({ pushTo, selectProd, setSelectedProd, addToCart, prod }) => {
 
           <span className="font-semibold">${price}.00</span>
           <div className="flex gap-3 my-2">
-          
+            <ProdCardGallery
+              selectProd={selectProd}
+              setSelectedProd={setSelectedProd}
+              prodImages={prodImages}
+            />
           </div>
           <div className="lg:hidden md:hidden block group-hover:block ">
             <Button
