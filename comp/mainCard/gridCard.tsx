88e94defa45a37fb7e0813f6b95c0499bfefa455
eslamfoda <eslaamfodaa@gmail.com>
2022-09-01 14:38:13 +0300

    debug

--- comp/mainCard/gridCard.tsx
+++ comp/mainCard/gridCard.tsx
@@ -11,37 +11,7 @@ const GridCard = ({ pushTo, selectProd, setSelectedProd, addToCart, prod }) => {
     <>
       {" "}
       <div className="overflow-hidden group h-[450px] lg:h-[550px] md:h-[550px] sm:h-[550px]">
-        <div className="top cursor-pointer  relative h-64 lg:h-96 md:h-96 sm:h-96">
-         
-        </div>
-        <div className="buttom group-hover:translate-y-[-60px] lg:translate-y-0 md:translate-y-0 translate-y-[-60px]  transition duration-300 pt-3 bg-white flex items-center justify-center flex-col gap-1">
-          <span className="text-gray-500 transition-all hover:text-primary-hover text-sm">
-            {vendor}
-          </span>
-          <Rate rate={rate} />
-
-          <span
-            onClick={pushTo}
-            className="text-center transition-all hover:text-primary-hover cursor-pointer  text-sm"
-          >
-            {title}
-          </span>
-
-          <span className="font-semibold">${price}.00</span>
-          <div className="flex gap-3 my-2">
-          
-          </div>
-          <div className="lg:hidden md:hidden block group-hover:block ">
-            <Button
-              onClick={addToCart}
-              icon={<CartIcon />}
-              height={40}
-              className="!p-4 text-xs"
-            >
-              ADD TO CART
-            </Button>
-          </div>
-        </div>
+      
       </div>
     </>
   );
